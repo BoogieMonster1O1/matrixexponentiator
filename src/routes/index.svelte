b4cf0d3e23ef09f4650c97cca5e5d894104d7604
SD <harrypotter.starwars.shrish@gmail.com>
2022-07-07 21:56:30 +0530

    basic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let order: number = 2;
+
+</script>
+
+<div class="flex flex-col justify-center items-center">
+    <div class="flex text-6xl text-center">
+        <h1>Matrix exponentiator</h1>
+    </div>
+    <h1>why would you ever raise e to the power of a matrix</h1>
+    <br>
+    <br>
+    <input type="number" bind:value={order} class="border-2 text-2xl" placeholder="Matrix length">
+    <br>
+
+</div>
